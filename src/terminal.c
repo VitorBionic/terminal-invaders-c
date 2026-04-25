4d4197ae7f31cb2440dd027988db8ec1deea431a
vitorbionic <vitorbionic@hotmail.com>
2026-04-25 19:53:36 -0300

    terminal raw mode complete

--- src/terminal.c
+++ src/terminal.c
@@ -13,22 +13,29 @@ int terminal_init() {
     fd = open("/dev/tty", O_RDWR);
     if (fd == -1) {
         fprintf(stderr, "%s: %s\n", "open", strerror(errno));
+        fd = -1;
         return -1;
     }
 
     if (tcgetattr(fd, &original) == -1) {
         fprintf(stderr, "%s: %s\n", "tcgetattr", strerror(errno));
         close(fd);
+        fd = -1;
         return -1;
     }
 
     raw = original;
 
     raw.c_lflag &= ~(ECHO | ICANON);
+    raw.c_iflag &= ~(IXON | ICRNL);
+    raw.c_oflag &= ~OPOST;
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 0;
 
     if (tcsetattr(fd, TCSAFLUSH, &raw) == -1) {
         fprintf(stderr, "%s: %s\n", "tcsetattr", strerror(errno));
         close(fd);
+        fd = -1;
         return -1;
     }
 
